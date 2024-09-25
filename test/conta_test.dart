5a04128d8a65ab513a2d89b86ca9a2a0a2051249
Gabriel <gabrielbcds40@gmail.com>
2024-09-25 11:05:47 -0300

    primeiro commit

--- test/conta_test.dart
+++ test/conta_test.dart
@@ -3,10 +3,18 @@ import 'package:atividadeflutter2/conta.dart';
 
 void main() {
   final conta = Conta<double>('João', 0.0); 
+  final conta2 = Conta<double>('João', 0.0); 
 
   conta.adicionarTransacao(100.0); 
   conta.adicionarTransacao(50.0); 
 
+  conta2.adicionarTransacao(1000.0); 
+  conta2.adicionarTransacao(500.0); 
+   conta2.adicionarTransacao(50.0);
+
+  
   print('Saldo Atual: ${conta.calcularSaldo()}'); 
   print('Transações: ${conta.getTransacoes()}'); 
+  print('Saldo Atual: ${conta2.calcularSaldo()}'); 
+  print('Transações: ${conta2.getTransacoes()}'); 
 }
